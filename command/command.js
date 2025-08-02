580a50ea13659b1ca89341ce6bf003f161bb9027
GreySole <101295876+GreySole@users.noreply.github.com>
2025-08-02 16:24:36 -0500

    Change export method

--- command/command.js
+++ command/command.js
@@ -1,7 +1,9 @@
-export default class SamplePlugin {
+class SamplePlugin {
   onLoad() {}
   onChat(message) {}
   onOSC(message) {}
   onEvent(type, event) {}
   onCommunityChat(type, event) {}
 }
+
+module.exports = SamplePlugin;
